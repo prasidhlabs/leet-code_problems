b0e8758c351854870bf142bac530fc3b16c26fda
PRASIDH T PRATHEESH <prasidh.pratheesh@gmail.com>
2026-09-23 09:34:22 +0530

    Implement valid parenthesis checker in Python

---
+++ Stack & Queue/valid_paranthesis.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in s:
+            if i=='(':
+                stack.append(')')
+            elif i=='{':
+                stack.append('}')
+            elif i=='[':
+                stack.append(']')
+            elif len(stack)==0 or stack.pop()!=i:
+                return False
+
+        return len(stack)==0
+
+        
